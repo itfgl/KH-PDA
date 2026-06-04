3c7e3335009b3fd369b02cd5165fed4c5f4056d6
185499680 <185499680@qq.com>
2026-06-04 11:01:50 +0800

    feat: ZXing fallback for camera scan, fix copy button with execCommand

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { registerPlugin } from '@capacitor/core';
+import { BrowserMultiFormatReader } from '@zxing/browser';
 
-window.ScanPlugin   = registerPlugin('ScanPlugin');
-window.PrintPlugin  = registerPlugin('PrintPlugin');
-window.KaihangNfc   = registerPlugin('KaihangNfc');
-window.BUILD_TIME   = __BUILD_TIME__;
+window.ScanPlugin        = registerPlugin('ScanPlugin');
+window.PrintPlugin       = registerPlugin('PrintPlugin');
+window.KaihangNfc        = registerPlugin('KaihangNfc');
+window.BUILD_TIME        = __BUILD_TIME__;
+window.ZXingReader       = BrowserMultiFormatReader;
